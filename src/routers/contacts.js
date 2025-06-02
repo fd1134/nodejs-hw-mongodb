d86e68554227850f631a75a07e4aadea2adf53d4
Fatih Demir <fd1134@gmail.com>
2025-06-02 22:16:23 +0300

    add deleteContactController

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,6 +1,12 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { getAllContactsController, getContactByIdController,postContactController ,patchContactController} from '../controllers/contacts.js';
+import {
+  getAllContactsController,
+  getContactByIdController,
+  postContactController,
+  patchContactController,
+  deleteContactController,
+} from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
@@ -9,5 +15,6 @@ router.get('/contacts', ctrlWrapper(getAllContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 router.post('/contacts', ctrlWrapper(postContactController));
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
