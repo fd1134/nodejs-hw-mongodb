801acd48aaf1fd28366193de220b032d429061a9
Fatih Demir <fd1134@gmail.com>
2025-07-04 00:21:24 +0300

    add email services

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { registerUserSchema ,loginUserSchema } from '../validation/auth.js';
-import { registerUserController,loginUserController,logoutUserController ,refreshUserSessionController } from '../controllers/auth.js';
+import { registerUserSchema ,loginUserSchema,requestResetEmailSchema,resetPasswordSchema } from '../validation/auth.js';
+import { registerUserController,loginUserController,logoutUserController ,refreshUserSessionController,requestResetEmailController,resetPasswordController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
 
 const router = Router();
@@ -10,5 +10,7 @@ router.post( '/register', validateBody(registerUserSchema), ctrlWrapper(register
 router.post( '/login',validateBody(loginUserSchema),ctrlWrapper(loginUserController),);
 router.post('/logout', ctrlWrapper(logoutUserController));
 router.post('/refresh',ctrlWrapper(refreshUserSessionController));
+router.post( '/request-reset-email',validateBody(requestResetEmailSchema),ctrlWrapper(requestResetEmailController),);
+router.post( '/reset-password',validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController),);
 
 export default router;
