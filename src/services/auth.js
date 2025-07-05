da9b500444b1a4da55d81e97caa9b73ba5bd0d46
Fatih Demir <fd1134@gmail.com>
2025-07-05 11:36:33 +0300

    reset password and secion deleted

--- src/services/auth.js
+++ src/services/auth.js
@@ -160,4 +160,7 @@ export const resetPassword = async (payload) => {
     { _id: user._id },
     { password: encryptedPassword },
   );
+
+  await SessionsCollection.deleteOne({ userId: user._id });
+
 };
